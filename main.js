76e2410232023e693c0800c7d66457aa1bbad650
bharatpipaliya <51047290+bharatpipaliya@users.noreply.github.com>
2023-05-16 16:50:34 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+var i=0;
+
+document.getElementById("b2").addEventListener("click",btn2);
+document.getElementById("b3").addEventListener("click",btn3);
+document.getElementById("b4").addEventListener("click",btn4);
+document.getElementById("b5").addEventListener("click",btn5);
+document.getElementById("b6").addEventListener("click",btn6);
+document.getElementById("b7").addEventListener("click",btn7);
+document.getElementById("b8").addEventListener("click",btn8);
+document.getElementById("b9").addEventListener("click",btn9);
+
+
+function btn2()
+{
+    document.getElementById("h11").innerHTML=(i+=2)
+}
+
+function btn3()
+{
+    document.getElementById("h11").innerHTML=(i+=3)
+}
+function btn4()
+{
+    document.getElementById("h11").innerHTML=(i+=4)
+}
+function btn5()
+{
+    document.getElementById("h11").innerHTML=(i+=5)
+}
+function btn6()
+{
+    document.getElementById("h11").innerHTML=(i+=6)
+}
+function btn7()
+{
+    document.getElementById("h11").innerHTML=(i+=7)
+}
+function btn8()
+{
+    document.getElementById("h11").innerHTML=(i+=8)
+}
+function btn9()
+{
+    document.getElementById("h11").innerHTML=(i+=9)
+}
